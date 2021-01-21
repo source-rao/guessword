26402c79d0a3918f4cd3d342afdb1355e2b4239d
Nikhil <sourcenrao@gmail.com>
2021-01-21 15:17:45 -0500

    Add incorrect guess history tracker, move victory code into while loop

--- guessword.py
+++ guessword.py
@@ -23,7 +23,7 @@ for letter in letters:
 # Variables
 incorrect = 0
 guesstest = 0
-win = 0
+incorrectguesses = list()
 
 print('The word has ' + str(len(letters)) + ' letters in it.')
 
@@ -31,24 +31,25 @@ while incorrect < 5:
     print('You have ' + str(5 - incorrect) +' incorrect guesses remaining.')
     guess = input('Enter a letter to guess: ').lower()
     count = 0
-    for letter in letters:
+    for letter in letters: # Check each letter against guessed character
         if guess == letter:
-            result[count] = letters[count]
-            guesstest += 1
-            count += 1
+            result[count] = letters[count] # Replace '_' with correctly guessed character
+            guesstest += 1 # Counter verifying if a guess was correct
+            count += 1 # Move position in result array
         else:
             count += 1
-    if guesstest == 0:
-        incorrect += 1
+    if guesstest == 0: # If incorrect guess
+        incorrect += 1 # Increment incorrect guess count
+        incorrectguesses.append(guess) # Add incorrect guess to guessed list
+    elif result.count('_') == 0: # Elif no blanks remaining
+        print('Congratulations! You guessed ' + word + '!')
+        break
     else:
-        if result.count('_') == 0:
-            win = 1
-            break
-        else:
-            guesstest = 0
-    print(' '.join(result))
+        guesstest = 0 # Reset correct guess verifier
+    print(' '.join(result)) # Print result
+    if incorrect > 0:
+        guessedstr = ' '.join(incorrectguesses) # Stringify list of incorrect guesses
+        print('Incorrect guesses: ' + guessedstr)
 
-if incorrect > 4 and win != 1:
+if incorrect > 4:
     print('Sorry, the word was ' + word + '!')
-else:
-    print('Congratulations! You guessed ' + word + '!')
